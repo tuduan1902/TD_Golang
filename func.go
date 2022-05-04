65f8a223f882e636ab49949a1389e9101f9a368d
Tu Duan <uuxinhxinh2701@gmail.com>
2022-05-04 22:28:06 +0700

    commit

---
+++ func.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func greeting(name string) string {
+	result := fmt.Sprintf("Hello %s", name)
+	return result
+}
+
+func main() {
+	w, h, isSquare := rectInfo(100, 200)
+	if isSquare {
+		fmt.Println("This is square")
+	} else {
+		fmt.Println("width=", w)
+		fmt.Println("heigth", h)
+	}
+}
+
+// Multiple return values
+// bool isSquare = true when width = height
+func rectInfo(w, h int) (width int, height int, isSquare bool) {
+	isSquare = w == h
+	return w, h, isSquare
+
+}
